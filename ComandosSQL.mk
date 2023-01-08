04da060df093f4c6e90044db46e057b0a5d660cc
João Paulo Barbosa Anastácio <89879181+Mamede5151@users.noreply.github.com>
2023-01-07 21:16:51 -0300

    Add files via upload

---
+++ ComandosSQL.mk
@@ -0,0 +1,11 @@
+#SELECT - utilizado para selecionar dados de uma ou mais tabelas
+#INSERT - utilizado para inserir novos registros em uma tabela
+#UPDATE - utilizado para atualizar dados de um ou mais registros de uma tabela
+#DELETE - utilizado para deletar um ou mais registros de uma tabela
+#CREATE TABLE - utilizado para criar uma nova tabela no banco de dados
+#ALTER TABLE - utilizado para modificar a estrutura de uma tabela existente
+#DROP TABLE - utilizado para deletar uma tabela do banco de dados
+#CREATE INDEX - utilizado para criar um índice em uma tabela para melhorar o desempenho de consultas
+#DROP INDEX - utilizado para deletar um índice de uma tabela
+#CREATE DATABASE - utilizado para criar um novo banco de dados
+#DROP DATABASE - utilizado para deletar um banco de dados existente
